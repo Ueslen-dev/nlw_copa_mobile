a6ee3f50962a0c1b04137b0a20224a18cb910782
Ueslen Santana <ueslen.santana@outlook.com>
2022-11-02 10:37:40 -0300

    feat: create screen signing

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,17 +1,32 @@
-import { Text, Center } from "native-base";
+import { Center, Icon, Text } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
+
+import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button";
 
 export function Signin() {
     return (
         <Center 
             flex={1} 
             bgColor="black"
+            p={7}
         >
+            <Logo width={212} height={40}/>
+
+            <Button 
+                title="ENTRAR COM O GOOGLE"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+                type="SECONDARY"
+                mt={12}
+            />
+
             <Text 
-                color="white" 
-                fontSize={24}
-                fontFamily="heading"
+                color="white"
+                textAlign="center"
+                mt={4}
             >
-               Signin
+                Não utilizamos nenhuma informação além  {'\n'}
+                do seu e-mail para criação de sua conta.
             </Text>
         </Center>
     )
